94f68dcdcba59a2c16e0d60d0403ff838415a72f
Brendan Richards <brenrichrads@byui.edu>
2024-03-14 17:09:03 -0600

    implemented alias to run /bin/sysinfo-bash/main.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "Welcome to sysinfo-bash, make sure you are in the directory with 'install.sh' before proceeding."
 
 contents=$(ls)
@@ -9,21 +10,21 @@ read choice
 
 case $choice in
     y|Y) 
+        CURRENT_DIR="$(pwd)"
+        DEST_DIR="/bin/sysinfo-bash"
 
-    CURRENT_DIR="$(pwd)"
-    DEST_DIR="/bin/sysinfo-bash"
+        mkdir -p "$DEST_DIR"
+        cp -r "$CURRENT_DIR"/* "$DEST_DIR"/
+        chmod +x "$DEST_DIR"/*
 
-
-    mkdir -p "$DEST_DIR"
-    cp -r "$CURRENT_DIR"/* "$DEST_DIR"/
-    chmod +x "$DEST_DIR"/*
-
-    echo "alias sysinfo='/bin/sysinfo-bash/main.sh'" >> ~/.bashrc
-    source ~/.bashrc
-    ;;
-
+        Make_Alias="alias sysinfo='/bin/sysinfo-bash/main.sh'"
+        rc_settingsILE="/etc/bash.bashrc"
+        echo "$Make_Alias" >> "$rc_settingsILE"
+        source "$rc_settingsILE"
+        echo "Alias 'sysinfo' added to $rc_settingsILE and applied."
+        echo "Run "sysinfo" to run sysinfo."
+        ;;
     n|N)
-    echo "Please navigate to the directory containing 'install.sh'"
-    ;;
-
+        echo "Please navigate to the directory containing 'install.sh'"
+        ;;
 esac
